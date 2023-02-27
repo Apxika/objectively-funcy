a42d730ae9a027803cbeed616b9dab21886791f2
Apxika <konstantine.paloumbis@gmail.com>
2023-02-27 11:07:25 -0500

    Finished

--- main.js
+++ main.js
@@ -1,8 +1,54 @@
 // Your code here.
 
+function getFirstName(obj) {
+  return obj.firstName
+}
 
+function getLastName(obj) {
+  return obj.lastName
+}
 
+function getFullName(obj) {
+  let fullName = obj.firstName + " " + obj.lastName
+  return fullName
+}
 
+function setFirstName(obj, name) {
+  obj.firstName = name
+  return obj.firstName
+}
+
+function setAge(obj, age) {
+  obj.age = age
+  return obj.age
+}
+
+function giveBirthday(obj) {
+  if (obj.age === undefined) {
+    obj.age = 1
+  } else {
+  obj.age = obj.age+1
+  }
+  return obj.age
+} 
+
+function marry(obj1, obj2) {
+  obj1.married = true
+  obj2.married = true
+  obj1.spouseName = obj2.firstName + ' ' + obj2.lastName
+  obj2.spouseName = obj1.firstName + ' ' + obj1.lastName
+  return obj1, obj2
+}
+
+function divorce(obj1, obj2) {
+  obj1.married = false
+  obj2.married = false
+  obj2.spouseName = false
+  obj1.spouseName = false
+  delete obj2.spouseName
+  delete obj1.spouseName
+  return obj1, obj2
+}
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
